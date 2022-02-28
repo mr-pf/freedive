68c5a91d8c9caa5e4371c42b2223079d4f9b67e0
Marc Pfander <marc.pfander@gmail.com>
2022-02-28 07:43:52 -0500

    debugging cors issue

--- freefall-solver/src/main.py
+++ freefall-solver/src/main.py
@@ -8,29 +8,8 @@ from json_parsing import parse_diver_case
 from solver import solve_diver_case
 
 
-@cross_origin()
+@cross_origin(allowed_methods=['POST'])
 def freefall_solver(request: flask.Request):
-    # For more information about CORS and CORS preflight requests, see:
-    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
-        }
-
-        return ('', 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
-
     try:
         request_data = request.get_json()
 
@@ -42,4 +21,4 @@ def freefall_solver(request: flask.Request):
         return response_data
 
     except Exception:
-        return traceback.format_exc(), 500, headers
+        return traceback.format_exc(), 500
